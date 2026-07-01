2383c39ac0f9e9930870cd5ac003309261e52953
Denys Valchuk <dvalchuk@gmail.com>
2026-07-01 14:37:11 +0300

    feat(python): add 0021 merge-two-sorted-lists solution and tests

---
+++ python/problems/0021-merge-two-sorted-lists/solution.py
@@ -0,0 +1,27 @@
+# Copyright (C) Denys Valchuk - All Rights Reserved
+# ZHZhbGNodWtAZ21haWwuY29tCg==
+
+from __future__ import annotations
+
+
+class ListNode:
+    def __init__(self, val: int = 0, next: ListNode | None = None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    
+    def mergeTwoLists(self, list1: ListNode | None, list2: ListNode | None) -> ListNode | None:
+        dummy = ListNode()
+        rit = dummy
+        while list1 and list2:
+            if list1.val < list2.val:
+                rit.next = list1
+                list1 = list1.next
+            else:
+                rit.next = list2
+                list2 = list2.next
+            rit = rit.next
+        rit.next = list1 or list2
+        return dummy.next
---
+++ python/problems/0021-merge-two-sorted-lists/test_solution.py
@@ -0,0 +1,48 @@
+# Copyright (C) Denys Valchuk - All Rights Reserved
+# ZHZhbGNodWtAZ21haWwuY29tCg==
+
+from solution import ListNode, Solution
+
+
+def make_list(vals: list[int]) -> ListNode | None:
+    dummy = ListNode()
+    cur = dummy
+    for v in vals:
+        cur.next = ListNode(v)
+        cur = cur.next
+    return dummy.next
+
+
+def to_list(node: ListNode | None) -> list[int]:
+    result = []
+    while node:
+        result.append(node.val)
+        node = node.next
+    return result
+
+
+s = Solution()
+
+
+def test_example1():
+    assert to_list(s.mergeTwoLists(make_list([1, 2, 4]), make_list([1, 3, 4]))) == [1, 1, 2, 3, 4, 4]
+
+
+def test_example2_both_empty():
+    assert to_list(s.mergeTwoLists(None, None)) == []
+
+
+def test_example3_one_empty():
+    assert to_list(s.mergeTwoLists(None, make_list([0]))) == [0]
+
+
+def test_first_list_empty():
+    assert to_list(s.mergeTwoLists(make_list([1, 3, 5]), None)) == [1, 3, 5]
+
+
+def test_single_elements():
+    assert to_list(s.mergeTwoLists(make_list([2]), make_list([1]))) == [1, 2]
+
+
+def test_all_same():
+    assert to_list(s.mergeTwoLists(make_list([1, 1]), make_list([1, 1]))) == [1, 1, 1, 1]
